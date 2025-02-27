d6f2d721b718be01a5c20d020466e4340c7a8653
B1229016 <93651509+LAWRENCECHIA93@users.noreply.github.com>
2025-02-27 14:06:16 +0800

    Create main3.c

---
+++ qz1/main3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+    int main(){
+      unsigned char i = 200;
+      unsigned char j = 220;
+      unsigned char k = 0;
+
+      k = i/2 + j/2 ;
+      printf("(%d + %d)/2 = %d\n", i,j,k);
+      return 0;
+    }
